084e76e47be2dde5a48f60bc434b9b00406a9fd1
권순성 <tnstjddlrl@naver.com>
2021-01-15 16:25:53 +0900

    리뷰관리 및 결제관리 완성후 커밋

--- payManage.js
+++ payManage.js
@@ -14,6 +14,7 @@ const chartWidth = Dimensions.get('window').width;
 
 const event = require('./img/eventBg.jpg')
 const arrow = require('./img/arrow02.png')
+const starimg =require('./img/review.png')
 
 import FootTer from './footer.js'
 import HeadHeder from "./header.js";
@@ -24,13 +25,22 @@ const PayManage = () => {
   return(
     <View>
       <View style={{height:chartHeight,width:chartWidth}}>
-        <ScrollView>
+        <ScrollView style={{backgroundColor:'white'}}>
           <View style={{marginBottom:500}}>
                       <View style={{width:chartWidth,marginTop:50}}>
                         <ImageBackground source={event} style={{width:chartWidth,height:chartHeight/7}}>
                         </ImageBackground>
                         <Text style={{position:'absolute',color:"white",fontSize:20,fontWeight:'bold',top:40,left:10}}>결제관리</Text>
                       </View>
+
+              <View style={{margin:10}}>
+
+                <ListItem></ListItem>
+                <ListItem></ListItem>
+                <ListItem></ListItem>
+                <ListItem></ListItem>
+
+              </View>
 
 
           </View>
@@ -43,5 +53,27 @@ const PayManage = () => {
     </View>
   )
 }
+
+const ListItem = () =>{
+  return(
+    <View style={{borderWidth:0.6,borderColor:'gray',borderRadius:17,marginTop:10}}>
+                <View style={{flexDirection:'row',alignItems:'center',width:chartWidth/1.15,margin:15}}>
+                  <Text>일반견적 - 시공완료</Text>
+                  <View style={{right:0,position:'absolute'}}>
+                      <Text>김업체</Text>
+                  </View>
+                </View>
+                <Text style={{marginLeft:10,marginRight:10}} numberOfLines={3}>전기 공사좀 해주세요</Text>
+                <View style={{flexDirection:'row',alignItems:'center'}}>
+                  <View style={{height:25,backgroundColor:'black',margin:10,borderRadius:5}}>
+                    <Text style={{color:'white',alignSelf:'center',margin:3}}>견적서보기</Text>
+                  </View>
+                  <Text style={{position:'absolute', right:15}}>2020-12-22 11:52:51</Text>
+                </View>
+              </View>
+  )
+}
+
+
 
 export default PayManage;
--- reviewManage.js
+++ reviewManage.js
@@ -14,6 +14,7 @@ const chartWidth = Dimensions.get('window').width;
 
 const event = require('./img/eventBg.jpg')
 const arrow = require('./img/arrow02.png')
+const starimg =require('./img/review.png')
 
 import FootTer from './footer.js'
 import HeadHeder from "./header.js";
@@ -24,13 +25,22 @@ const ReviewManage = () => {
   return(
     <View>
       <View style={{height:chartHeight,width:chartWidth}}>
-        <ScrollView>
+        <ScrollView style={{backgroundColor:'white'}}>
           <View style={{marginBottom:500}}>
                       <View style={{width:chartWidth,marginTop:50}}>
                         <ImageBackground source={event} style={{width:chartWidth,height:chartHeight/7}}>
                         </ImageBackground>
                         <Text style={{position:'absolute',color:"white",fontSize:20,fontWeight:'bold',top:40,left:10}}>리뷰관리</Text>
                       </View>
+
+            <View style={{margin:10}}>
+
+              <ListItem></ListItem>
+              <ListItem></ListItem>
+              <ListItem></ListItem>
+              <ListItem></ListItem>
+
+            </View>
 
 
           </View>
@@ -41,6 +51,30 @@ const ReviewManage = () => {
       <FootTer></FootTer>
 
     </View>
+  )
+}
+
+const ListItem = () =>{
+  return(
+    <View style={{borderWidth:0.6,borderColor:'gray',borderRadius:17,marginTop:10}}>
+                <View style={{flexDirection:'row',alignItems:'center',width:chartWidth/1.15,margin:15}}>
+                  <Text>김업체</Text>
+                  <View style={{flexDirection:'row',right:0,position:'absolute'}}>
+                      <Image source={starimg} style={{width:15,height:15}}></Image>
+                      <Image source={starimg} style={{width:15,height:15}}></Image>
+                      <Image source={starimg} style={{width:15,height:15}}></Image>
+                      <Image source={starimg} style={{width:15,height:15}}></Image>
+                      <Image source={starimg} style={{width:15,height:15}}></Image>
+                  </View>
+                </View>
+                <Text style={{marginLeft:10,marginRight:10}} numberOfLines={3}>거실등이 인테리어랑 맞지 않아서 새로 전등을 교체하려고 하니 셀프로 하기엔 무리가 있었습니다. 견적의뢰하고 다음날 바로 시공진행해서 너무 만족합니다~!</Text>
+                <View style={{flexDirection:'row',alignItems:'center'}}>
+                  <View style={{height:25,backgroundColor:'black',margin:10,borderRadius:5}}>
+                    <Text style={{color:'white',alignSelf:'center',margin:3}}>견적서보기</Text>
+                  </View>
+                  <Text style={{position:'absolute', right:15}}>2020-12-22 11:52:51</Text>
+                </View>
+              </View>
   )
 }
 
