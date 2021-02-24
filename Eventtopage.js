f5fb3d749a62eae0b1430c947b358a0b9740b6bd
권순성 <tnstjddlrl@naver.com>
2021-02-24 12:02:14 +0900

    이벤트 자세히보기 마진바텀값 수정

--- Eventtopage.js
+++ Eventtopage.js
@@ -60,7 +60,7 @@ const EventToPage = ({route}) => {
     <View>
       <View style={{height:chartHeight,width:chartWidth}}>
         <ScrollView>
-          <View style={{marginBottom:500}}>
+          <View style={{marginBottom:100}}>
                       <View style={{width:chartWidth,marginTop:50}}>
                         <ImageBackground source={event} style={{width:chartWidth,height:chartHeight/7}}>
                         </ImageBackground>
