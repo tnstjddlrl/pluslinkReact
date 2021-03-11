a949e0ca613cbb697f5dd67bd9644c467b26387d
권순성 <tnstjddlrl@naver.com>
2021-03-11 16:23:29 +0900

    ios폰트 적용은 다음에

--- Banner.js
+++ Banner.js
@@ -11,6 +11,7 @@ import {
   TouchableOpacity,
   Button,
   TextInput,
+  Platform,
 } from "react-native";
 const chartHeight = Dimensions.get('window').height;
 const chartWidth = Dimensions.get('window').width;
@@ -29,29 +30,83 @@ const ba1 = require('./img/ba1.jpg')
 const ba2 = require('./img/ba2.jpg')
 const ba3 = require('./img/ba3.jpg')
 
+let os = Platform.OS
+
+const arrow = require('./img/arrow02.png')
+
+
 const Banner = ()=>{
+  const [isis,setIsis] = useState('android');
+
+  useEffect(()=>{
+    if (os == 'ios') {
+      setIsis('ios')
+    } else {
+    }
+  },[])
+  
+
+  
   return(
     // 스와이퍼 오픈소스를 활용하여 메인 배너 구성, 구성 요소는 ImageBackground로 구성한다.
-    <Swiper style={{marginTop:20,height:chartHeight/4.5,}} showsButtons={false} autoplay={true} autoplayTimeout={4}>
+    <Swiper style={{marginTop:20,height:chartHeight/3.8,}} showsButtons={false} autoplay={true} autoplayTimeout={4}>
         
-        <ImageBackground style={{width:chartWidth,height:chartHeight/4.5}} source={ba1}>
+        <ImageBackground style={{width:chartWidth,height:chartHeight/3.8}} source={ba1}>
           <View style={{width:chartWidth,height:chartHeight/4,top:"20%",left:'3%'}}>
-            <Text style={{color:'white',fontFamily:'ajh',fontSize:18}} >당장 시공이 필요할 때, 프링으로 손 쉽게 신청하세요!</Text>
-            <Text style={{color:'white',fontFamily:'regular',fontSize:12}} >당장 시공이 필요할 때, 프링으로 손 쉽게 신청하세요!</Text>
+            {isis == 'android' ?
+            <View style={{marginLeft:10}}>
+            <Text style={{color:'white',fontFamily:'ajh',fontSize:18}} >당장 시공이 필요할 때,</Text>
+            <Text style={{color:'white',fontFamily:'ajh',fontSize:18,marginTop:5}} >프링으로 손 쉽게 신청하세요!</Text>
+            <Text style={{color:'white',fontFamily:'regular',fontSize:12}} >프링 어플로 손 쉽게 견적신청하세요</Text>
+            <View style={{backgroundColor:'white',borderRadius:20,width:120,height:28,justifyContent:"center",alignItems:"center",top:'10%' , flexDirection:"row"}}>
+              <Text style={{fontSize:11,fontFamily:'regular',letterSpacing:-1,fontWeight:'bold'}}>견적신청 바로가기</Text>
+            </View>
+            </View>
+            :
+            <View style={{marginTop:50}}>
+            <Text style={{color:'white',fontSize:18,fontWeight:"bold"}} >당장 시공이 필요할 때, 프링으로 손 쉽게 신청하세요!</Text>
+            <Text style={{color:'white',fontSize:12}} >프링 어플로 손 쉽게 견적신청하세요</Text>
+            </View>
+            }
           </View>
         </ImageBackground>
 
-        <ImageBackground style={{width:chartWidth,height:chartHeight/4.5}} source={ba2}>
+        <ImageBackground style={{width:chartWidth,height:chartHeight/3.8}} source={ba2}>
           <View style={{width:chartWidth,height:chartHeight/4,top:"20%",left:'3%'}}>
-          <Text style={{color:'white',fontFamily:'ajh',fontSize:18}} >후기로 증명된 프링의 간편함, 지금 체험하세요!</Text>
-          <Text style={{color:'white',fontFamily:'regular',fontSize:12}} >지금 당장 내 주변 시공견적을 비교해보세요</Text>
+          {isis == 'android' ?
+            <View style={{marginLeft:10}}>
+            <Text style={{color:'white',fontFamily:'ajh',fontSize:18}} >후기로 증명된 프링의 간편함, 지금 체험하세요!</Text>
+            <Text style={{color:'white',fontFamily:'regular',fontSize:12}} >지금 당장 내 주변 시공견적을 비교해보세요</Text> 
+            <View style={{backgroundColor:'white',borderRadius:20,width:120,height:28,justifyContent:"center",alignItems:"center",top:'15%' , flexDirection:"row"}}>
+              <Text style={{fontSize:11,fontFamily:'regular',letterSpacing:-1,fontWeight:'bold'}}>회원가입 바로가기</Text>
+            </View>
+            </View>
+            :
+            <View style={{marginTop:50}}>
+            <Text style={{color:'white',fontSize:18,fontWeight:"bold"}} >후기로 증명된 프링의 간편함, 지금 체험하세요!</Text>
+            <Text style={{color:'white',fontSize:12}} >지금 당장 내 주변 시공견적을 비교해보세요</Text>
+            </View>
+          }
           </View>
         </ImageBackground>
 
-        <ImageBackground style={{width:chartWidth,height:chartHeight/4.5}} source={ba3}>
+        <ImageBackground style={{width:chartWidth,height:chartHeight/3.8}} source={ba3}>
           <View style={{width:chartWidth,height:chartHeight/4,top:"20%",left:'3%'}}>
-          <Text style={{color:'white',fontFamily:'ajh',fontSize:18}} >이사 후, 프링으로 인테리어를 한번에 해결하세요</Text>
-          <Text style={{color:'white',fontFamily:'regular',fontSize:12}} >프링몰에서 패키지 구매 후 시공까지 한번에!</Text>
+          {isis == 'android' ?
+            <View style={{marginLeft:10}}>
+            <Text style={{color:'white',fontFamily:'ajh',fontSize:18}} >이사 후, 프링으로 </Text>
+            <Text style={{color:'white',fontFamily:'ajh',fontSize:18,marginTop:5}} >인테리어를 한번에 해결하세요</Text>
+            <Text style={{color:'white',fontFamily:'regular',fontSize:12}} >프링몰에서 패키지 구매 후 시공까지 한번에!</Text> 
+            <View style={{backgroundColor:'white',borderRadius:20,width:110,height:28,justifyContent:"center",alignItems:"center",top:'10%' , flexDirection:"row"}}>
+              <Text style={{fontSize:11,fontFamily:'regular',letterSpacing:-1,fontWeight:'bold'}}>프링몰 바로가기</Text>
+            </View>
+            </View>
+            :
+            <View style={{marginTop:50}}>
+            <Text style={{color:'white',fontSize:18,fontWeight:"bold"}} >이사 후, 프링으로 인테리어를 한번에 해결하세요</Text>
+            <Text style={{color:'white',fontSize:12}} >프링몰에서 패키지 구매 후 시공까지 한번에!</Text>
+            </View>
+          }
           </View>
         </ImageBackground>
 
